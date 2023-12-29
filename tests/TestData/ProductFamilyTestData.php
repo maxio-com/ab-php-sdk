6f91dfb22fa1c47c80afb16a5d7fcf7c1ff98d28
SebastianMiklaszewskiBldr <sebastian.miklaszewski@boldare.com>
2023-12-29 12:17:06 +0100

    feat(php-sdk): add customer tests

--- tests/TestData/ProductFamilyTestData.php
+++ tests/TestData/ProductFamilyTestData.php
@@ -13,9 +13,8 @@ final class ProductFamilyTestData
     public const NAME_FIVE = 'test product family 5';
     public const NAME_SIX = 'test product family 6';
     public const NAME_SEVEN = 'test product family 7';
-    public const NAME_EIGHT = 'test product family 8';
     public const HANDLE_ONE = 'test-product-family-1';
-    public const HANDLE_FOUR = 'test-product-family-4';
+    public const HANDLE_THREE = 'test-product-family-3';
     public const DESCRIPTION = 'test description';
     public const ACCOUNTING_CODE = null;
 }
